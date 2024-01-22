008de147de2741c4501338c1b6382ece8f671346
Sorraimi Rivas <73683734+SorraimiRivas@users.noreply.github.com>
2024-01-22 16:14:33 -0400

    Revert "feat: main screen and skeleton"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,15 @@
-import { ApolloProvider } from "@apollo/client";
 import { Stack } from "expo-router";
-import client from "../graphql/client";
+
 
 export default function MainStack() {
-	return (
-		<ApolloProvider client={client}>
-			<Stack
-				screenOptions={{
-					headerShown: false,
-					animation: "flip",
-				}}
-			>
-				<Stack.Screen name="index" />
-				<Stack.Screen name="[id]" />
-				<Stack.Screen name="evolutions" />
-			</Stack>
-		</ApolloProvider>
-	);
+    return (
+        <Stack screenOptions={{
+            headerShown: false,
+            animation: "flip"
+        }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="[id]" />
+            <Stack.Screen name="evolutions" />
+        </Stack>
+    )
 }
